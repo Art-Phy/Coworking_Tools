a3b3a800a5d2f9178ba3b233c5fa40e74871c21d
ArturoGit <arturosanma@gmail.com>
2025-11-25 15:14:12 +0100

    feat: añadir schemas y CRUD completos para herramientas

---
+++ app/crud/__init__.py
@@ -0,0 +1,16 @@
+
+from .tool import (
+    get_tool_by_id,
+    get_all_tools,
+    create_tool,
+    update_tool,
+    delete_tool,
+)
+
+__all__ = [
+    "get_tool_by_id",
+    "get_all_tools",
+    "create_tool",
+    "update_tool",
+    "delete_tool",
+]
---
+++ app/schemas/__init__.py
@@ -0,0 +1,4 @@
+
+from .tool import ToolBase, ToolCreate, ToolUpdate, ToolResponse
+
+__all__ = ["ToolBase", "ToolCreate", "ToolUpdate", "ToolResponse"]
